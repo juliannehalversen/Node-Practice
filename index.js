3449ec4220feabc6e3565b13d720bf91fd6a7e22
Julianne Lee <juliannehalversen@gmail.com>
2020-01-22 13:38:50 -0700

    removed comments

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(bodyParser.urlencoded({
 
 app.use('/', testRoute)
 
-// const test_controller = require('../controllers/test.controller');
-
  app.use('/', function(req, res, next) {
     console.log(req);
     console.log(`A new request was received at ${new Date()
@@ -26,47 +24,3 @@ const port = 5000;
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 })
-
-  
-
-// PART WITHOUT EXPRESS
-
-/*  const http = require('http');
- const fs = require('fs');
- 
- const server = http.createServer((req, res) => {
-     const url = req.url;
-     const method = req.method;
- 
-     if (url === '/') {
-         res.write('<html>')
-         res.write('<body><h1>Welcome to my page!</body>')
-         res.write('</html>')
-         return res.end();
-     }
-     if (url === '/api' && method === 'POST') {
-         const body = [];
-         req.on('data', (chunk) => {
-             console.log(chunk);
-             body.push(chunk);
-         });
-         req.on('end', () => {
-             const parsedBody = Buffer.concat(body).toString();
-             const message = parsedBody.split('=')[1];
-             fs.writeFile('message.txt', message, () => {
-                 res.statusCode = 302;
-                 res.setHeader('Location', '/');
-                 return res.end();
-             });
-         });
-     }
- 
-     console.log(req.url, req.method, req.headers);
-     res.setHeader('Content-Type', 'text/html');
-     res.write('<html>')
-     res.write('<h1>Hello!</h1>')
-     res.write('</html>')
-     res.end();
- });
- 
- server.listen(5000);  */
